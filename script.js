8e9c618732e055819fa667e108765687ee31e60e
billkill <chathuradilsh97@gmail.com>
2026-06-01 09:47:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('clickBtn').addEventListener('click', function() {
+    document.getElementById('message').textContent = 'Button was clicked! Welcome to your new site.';
+    document.getElementById('heading').style.color = '#764ba2';
+});
